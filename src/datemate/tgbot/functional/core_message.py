544ada3806b5852f1ac3ce05c65cdfb5d7d7921f
Fedor Petrov <fppetrov@edu.hse.ru>
2025-11-28 02:04:53 +0300

    Добавил небольшой скелет проекта

---
+++ src/datemate/tgbot/functional/core_message.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+
+class CoreMessage:
+    """
+    Главное сообщение в диалоге, которое редактируется при каждом ответе пользователя
+
+    Используется принцип Single-Message-Dialog
+    """
+
+    def __init__(self, chat_id: int, message_id: int, telegram_id: int, date: datetime):
+        self.chat_id = chat_id
+        self.message_id = message_id
+        self.telegram_id = telegram_id
+        self.date = date
